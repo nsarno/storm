7e5b02c8c3d50a017ba58c42595da8d6e1c049fc
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-08-08 19:47:33 +0200

    add joken as a dependency

---
+++ config/joken.ex
@@ -0,0 +1,26 @@
+defmodule Winter.Config.Joken do
+  @behaviour Joken.Config
+
+  def secret_key(), do: Winter.Endpoint.config :secret_key_base
+
+  def algorithm(), do: :HS256
+
+  def encode(map), do: Poison.encode!(map)
+
+  def decode(binary), do: Poison.decode!(binary)
+
+  def claim(:exp, payload) do
+    Joken.Helpers.get_current_time() + 300
+  end
+
+  def claim(_, _), do: nil
+
+  def validate_claim(:exp, payload, options) do
+    Joken.Helpers.validate_time_claim payload, "exp", "Token expired",
+      fn(expires_at, now) ->
+        expires_at > now
+      end
+  end
+
+  def validate_claim(_, _, _), do: :ok
+end
