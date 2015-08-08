397f5901ae2a9478faa014a27b29b5be02a463d0
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-08-08 19:46:45 +0200

    adapt error view to JSON API only

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,17 +5,17 @@ defmodule Winter.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(Winter.ErrorView, "404.html", []) ==
-           "Page not found"
+    assert render_to_string(Winter.ErrorView, "404.json", []) ==
+           "{\"error\":\"Page not found\"}"
   end
 
   test "render 500.html" do
-    assert render_to_string(Winter.ErrorView, "500.html", []) ==
-           "Server internal error"
+    assert render_to_string(Winter.ErrorView, "500.json", []) ==
+           "{\"error\":\"Server internal error\"}"
   end
 
   test "render any other" do
-    assert render_to_string(Winter.ErrorView, "505.html", []) ==
-           "Server internal error"
+    assert render_to_string(Winter.ErrorView, "505.json", []) ==
+           "{\"error\":\"Server internal error\"}"
   end
 end
